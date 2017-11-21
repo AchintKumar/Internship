d3421b91b62039bbf35247e8bfaffb8ebee1b69a
Achint Kumar <to.achint.kumar@gmail.com>
2017-11-21 18:38:38 +0530

    Plots and more

--- Base.py
+++ Base.py
@@ -31,33 +31,44 @@ derm_raw.replace('?','')
 print(derm_raw.age.isnull)
 
 #Converting variables to respective data types
-derm_raw.erythema=derm_raw.erythema.astype('category')
-print(derm_raw.erythema)
+colnames=list(derm_raw.columns.values)
+colnames=colnames[:-2]
+print(colnames[0])
+for i in colnames:
+	derm_raw[i]=derm_raw[i].astype('category')
+print(derm_raw.dtypes)
+print(derm_raw.describe(include='all'))
+derm_raw.disease_type=derm_raw.disease_type.astype('category')
+print(derm_raw.disease_type)
+derm_raw.age[derm_raw.age=='?']='0'
+derm_raw.age=derm_raw.age.astype('float32')
+print(derm_raw.age[32:38])
 
 
 
 
+#Univariate Analysis of first 33 elements
+for i in colnames:
+	derm_raw[i].value_counts().plot.pie(autopct='%1.1f%%',labels=['','','',''],title=i)
+	plt.gca().set_aspect('equal')
+	plt.legend(labels=derm_raw.index)
+	plt.show()
+print(derm_raw.shape)
+ 
+#Of age
+derm_raw.age.plot.hist()
+plt.title('age')
+plt.show()
 
-
-
-#Univariate Analysis
-plt.hist(derm_raw.erythema)
+#Of disease type (different methos of plotting)
+a = pd.crosstab(derm_raw.disease_type,columns="count")
+a.plot(kind='pie',subplots=True,autopct='%1.1f%%',labels=['','','','','',''],title='disease_type')
+plt.gca().set_aspect('equal')
+plt.legend(loc=1,bbox_to_anchor=(1.5,1),labels=['Psoriasis','Seboreic Dermatitis','Lichen Planus','Pityriasis Rosea','Cronic Dermatitis','Pityriasis Rubra Pilaris'])
 plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#correlation
+#correlation (drawn before converting to categorical)
 corr=derm_raw.corr()
 print(corr)
 corr=pd.DataFrame(corr)
@@ -67,3 +78,9 @@ print(corr.dtypes)
 plt.subplots(figsize=(20,20))
 sns.heatmap(corr,vmax=1)
 plt.show()
+
+
+plt.boxplot(derm_raw.age)
+plt.title('Age')
+plt.show()
+
